4666b05e220f45b909ea273be584a3dc04cff3f0
Karen Sawrey <krnswry@gmail.com>
2017-05-02 23:24:54 +0300

    Codewars Keep Hydrated puzzle
    Nathan drinks water while cycling, and I'm storing this to hve a go-to file for a very simple rounding of floats to integers.

---
+++ Nathan_drinks_water.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  2 23:15:06 2017
+
+@author: Karen
+"""
+
+per_hour_of_cycling = 0.5
+hours_of_cycling = int(input("Input number of hours Nathan is cycling: "))
+ls_of_water = hours_of_cycling * per_hour_of_cycling
+print (int(ls_of_water))
